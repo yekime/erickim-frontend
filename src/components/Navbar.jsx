fc6534bdc63e64e5f3f527457528d074e517eb49
Eric Kim <yk742@cornell.edu>
2022-07-10 16:28:34 -0400

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/'></Link>-55
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar
